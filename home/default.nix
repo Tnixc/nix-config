df79f62bc9a78509a1d88f86a91d5e8a97a1973c
Tnixc <tnixxc@gmail.com>
2024-09-18 17:38:12 -0400

    update flake

--- home/default.nix
+++ home/default.nix
@@ -29,7 +29,7 @@
     # You can update Home Manager without changing this value. See
     # the Home Manager release notes for a list of state version
     # changes in each release.
-    stateVersion = "24.05";
+    stateVersion = "24.11";
   };
 
   # Let Home Manager install and manage itself.
