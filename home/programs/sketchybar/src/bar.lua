bc41f983019edc91555499ae10bbb20bd82f4e43
Tnixc <tnixxc@gmail.com>
2024-09-18 11:07:15 -0400

    sketchybar

--- home/programs/sketchybar/src/bar.lua
+++ home/programs/sketchybar/src/bar.lua
@@ -3,8 +3,8 @@ local colors = require("colors")
 -- Equivalent to the --bar domain
 sbar.bar({
 	topmost = "window",
-	height = 47,
+	height = 50,
 	color = colors.bar.bg,
 	padding_right = 3,
-	padding_left = 6,
+	padding_left = 9,
 })
