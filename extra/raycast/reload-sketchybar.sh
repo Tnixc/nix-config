3739b92eb731b39ecbbbad44101349b1fb369de0
Tnixc <tnixxc@gmail.com>
2024-09-27 14:47:43 -0400

    raycast

---
+++ extra/raycast/reload-sketchybar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Reload sketchybar
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon icon.png
+
+# Documentation:
+# @raycast.author tnixc
+# @raycast.authorURL https://raycast.com/tnixc
+
+export USER='tnixc'
+sketchybar --reload
+
