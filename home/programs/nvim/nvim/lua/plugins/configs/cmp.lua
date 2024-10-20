7ddf6704cf2a6d776e890ffee2999fa3227f9f53
Tnixc <tnixxc@gmail.com>
2024-10-19 21:16:04 -0400

    style nvim

--- home/programs/nvim/nvim/lua/plugins/configs/cmp.lua
+++ home/programs/nvim/nvim/lua/plugins/configs/cmp.lua
@@ -1,46 +1,46 @@
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 
-
 cmp.setup({
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "lazydev", group_index = 0 },
-    { name = "luasnip" },
-    { name = "path" },
-  },
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-    ["<C-j>"] = cmp.mapping.select_next_item(),
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-  }),
-  formatting = {
-    fields = { "abbr", "kind", "menu" },
-    format = function(entry, item)
-      local color_item = require("nvim-highlight-colors").format(entry, { kind = item.kind })
-      item = require("lspkind").cmp_format({})(entry, item)
-      if color_item.abbr_hl_group then
-        item.kind_hl_group = color_item.abbr_hl_group
-        item.kind = color_item.abbr
-      end
-      item.menu = ({
-        buffer = "[Buf]",
-        path = "[Path]",
-        nvim_lsp = "[LSP]",
-        luasnip = "[Snip]",
-        cmdline = "[Cmd]",
-        nvim_lua = "[Lua]",
-      })[entry.source.name]
-      return item
-    end,
-    expandable_indicator = true,
-  },
-  window = {
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "lazydev", group_index = 0 },
+		{ name = "luasnip" },
+		{ name = "path" },
+	},
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<Tab>"] = cmp.mapping.confirm({ select = true }),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+	}),
+	formatting = {
+		mode = "symbol",
+		fields = { "abbr", "kind", "menu" },
+		format = function(entry, item)
+			local color_item = require("nvim-highlight-colors").format(entry, { kind = item.kind })
+			item = require("lspkind").cmp_format({ mode = "symbol" })(entry, item)
+			if color_item.abbr_hl_group then
+				item.kind_hl_group = color_item.abbr_hl_group
+				item.kind = color_item.abbr
+			end
+			item.menu = ({
+				buffer = "[Buf]",
+				path = "[Path]",
+				nvim_lsp = "[LSP]",
+				luasnip = "[Snip]",
+				cmdline = "[Cmd]",
+				nvim_lua = "[Lua]",
+			})[entry.source.name]
+			return item
+		end,
+		expandable_indicator = true,
+	},
+	window = {
 		completion = {
 			border = Border("FloatBorder"),
 			winhighlight = "Normal:Normal,CursorLine:Visual,Search:None",
--- home/programs/nvim/nvim/lua/plugins/configs/lsp.lua
+++ home/programs/nvim/nvim/lua/plugins/configs/lsp.lua
@@ -1,87 +1,16 @@
--- Brief aside: **What is LSP?**
---
--- LSP is an initialism you've probably heard, but might not understand what it is.
---
--- LSP stands for Language Server Protocol. It's a protocol that helps editors
--- and language tooling communicate in a standardized fashion.
---
--- In general, you have a "server" which is some tool built to understand a particular
--- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
--- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
--- processes that communicate with some "client" - in this case, Neovim!
---
--- LSP provides Neovim with features like:
---  - Go to definition
---  - Find references
---  - Autocompletion
---  - Symbol Search
---  - and more!
---
--- Thus, Language Servers are external tools that must be installed separately from
--- Neovim. This is where `mason` and related plugins come into play.
---
--- If you're wondering about lsp vs treesitter, you can check out the wonderfully
--- and elegantly composed help section, `:help lsp-vs-treesitter`
-
---  This function gets run when an LSP attaches to a particular buffer.
---    That is to say, every time a new file is opened that is associated with
---    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
---    function will be executed to configure the current buffer
-
--- LSP servers and clients are able to communicate to each other what features they support.
---  By default, Neovim doesn't support everything that is in the LSP specification.
---  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
---  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. Available keys are:
---  - cmd (table): Override the default command used to start the server
---  - filetypes (table): Override the default list of associated filetypes for the server
---  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
---  - settings (table): Override the default settings passed when initializing the server.
---        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-local servers = {
-	-- clangd = {},
-	-- gopls = {},
-	-- pyright = {},
-	-- rust_analyzer = {},
-	-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-	--
-	-- Some languages (like typescript) have entire language plugins that can be useful:
-	--    https://github.com/pmizio/typescript-tools.nvim
-	--
-	-- But for many setups, the LSP (`tsserver`) will work just fine
-	-- tsserver = {},
-	--
-}
-
--- Ensure the servers and tools above are installed
---  To check the current status of installed tools and/or manually install
---  other tools, you can run
---    :Mason
---
---  You can press `g?` for help in this menu.
+-- ensure installed:
+local servers = {}
 require("mason").setup()
-
--- You can add other tools here that you want Mason to install
--- for you, so that they are available from within Neovim.
 local ensure_installed = vim.tbl_keys(servers or {})
 require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
--- NOTE: this fixes the split opening with zig diagnostics
 vim.g.zig_fmt_parse_errors = 0
 
 require("mason-lspconfig").setup({
 	handlers = {
 		function(server_name)
 			local server = servers[server_name] or {}
-			-- This handles overriding only values explicitly passed
-			-- by the server configuration above. Useful when disabling
-			-- certain features of an LSP (for example, turning off formatting for tsserver)
 			server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
 			require("lspconfig")[server_name].setup(server)
 		end,
