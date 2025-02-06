6466ec2a7bbb4d1f68af925f5d524b80bcb3f92a
Tnixc <tnixxc@gmail.com>
2025-02-06 01:52:21 -0500

    use sf icons

--- home/programs/nvim/nvim/lua/plugins/configs/telescope.lua
+++ home/programs/nvim/nvim/lua/plugins/configs/telescope.lua
@@ -22,7 +22,7 @@ require("telescope").setup({
 			"--column",
 			"--smart-case",
 		},
-		prompt_prefix = "   ",
+		prompt_prefix = " 􀊫  ",
 		selection_caret = "  ",
 		entry_prefix = "  ",
 		initial_mode = "insert",
