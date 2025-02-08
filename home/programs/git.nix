550b0d4598421f18c86b628e5db76867e82283a3
Tnixc <tnixxc@gmail.com>
2025-02-08 14:44:17 -0500

    update colors

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -25,8 +25,8 @@
           "#181616"
           "#0D0C0C"
           "#0A0909"
-          "#2A2A37"
-          "#3a3a4c"
+          "#363834"
+          "#4a4c47"
         ];
         dark = true;
         file-decoration-style = "#C5C9C5";
@@ -35,11 +35,11 @@
         hunk-header-file-style = "#23273D";
         hunk-header-line-number-style = "#23273D";
         hunk-header-style = "#23273D";
-        line-numbers-left-style = "#51516b";
+        line-numbers-left-style = "#686b64";
         line-numbers-minus-style = "bold #E46876";
         line-numbers-plus-style = "bold #87A987";
-        line-numbers-right-style = "#51516b";
-        line-numbers-zero-style = "#51516b";
+        line-numbers-right-style = "#686b64";
+        line-numbers-zero-style = "#686b64";
         minus-emph-style = "bold syntax #53394c";
         minus-style = "syntax #34293a";
         plus-emph-style = "bold syntax #404f4a";
