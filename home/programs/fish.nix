208e46eee09a79355471f220d4ba510754d07727
Tnixc <tnixxc@gmail.com>
2025-02-23 20:21:47 -0500

    fix: zed & fish

--- home/programs/fish.nix
+++ home/programs/fish.nix
@@ -45,6 +45,7 @@
       fish_add_path $HOME/.cargo/bin
       fish_add_path $HOME/.bun/bin
       fish_add_path $HOME/.local/bin
+      fish_add_path /usr/local/bin
       fish_add_path /opt/homebrew/bin
       fish_add_path /run/current-system/sw/bin
 
