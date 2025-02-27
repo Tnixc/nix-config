4118b0c6aa8600031afecf7e6deb2e22c962e1e1
Tnixc <tnixxc@gmail.com>
2025-02-27 15:58:30 -0500

    sketchybar battery icons

--- home/programs/aerospace-sketchybar/sbar-config-libs/items/widgets/battery.lua
+++ home/programs/aerospace-sketchybar/sbar-config-libs/items/widgets/battery.lua
@@ -43,27 +43,22 @@ battery:subscribe({ "routine", "power_source_change", "system_woke" }, function(
 		local color = colors.green
 		local charging, _, _ = batt_info:find("AC Power")
 
+		if found and charge > 80 then
+			icon = icons.battery._100
+		elseif found and charge > 60 then
+			icon = icons.battery._75
+		elseif found and charge > 40 then
+			icon = icons.battery._50
+		elseif found and charge > 20 then
+			icon = icons.battery._25
+			color = colors.orange
+		else
+			icon = icons.battery._0
+			color = colors.red
+		end
+
 		if charging then
 			icon = icons.battery.charging
-			if found and charge < 40 and charge > 20 then
-				color = colors.green
-			elseif found and charge > 20 then
-				color = colors.orange
-			elseif found and charge <= 20 then
-				color = colors.red
-			elseif found and charge > 80 then
-				icon = icons.battery._100
-			elseif found and charge > 60 then
-				icon = icons.battery._75
-			elseif found and charge > 40 then
-				icon = icons.battery._50
-			elseif found and charge > 20 then
-				icon = icons.battery._25
-				color = colors.orange
-			else
-				icon = icons.battery._0
-				color = colors.red
-			end
 		end
 
 		local lead = ""
