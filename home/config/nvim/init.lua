57fc807623ae987b5ed1525e648becbef8cfb1e1
Tnixc <tnixxc@gmail.com>
2025-04-22 10:03:33 -0400

    updates
    remove backup zed settings

--- home/config/nvim/init.lua
+++ home/config/nvim/init.lua
@@ -23,3 +23,23 @@ require("plugins")
 
 -- Keybindings
 require("core.mappings")
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+parser_config.elle = {
+	install_info = {
+		url = "~/Developer/tree-sitter-elle", -- local path or git repo
+		files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+		-- optional entries:
+		branch = "main", -- default branch in case of git repo if different from master
+		generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+		requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+	},
+	filetype = "le", -- if filetype does not match the parser name
+}
+
+vim.filetype.add({
+	extension = {
+		le = "le",
+	},
+})
