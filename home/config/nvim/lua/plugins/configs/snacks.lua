0de9a9c12c8f19aeb1783efc340f4183c08434ab
Tnixc <tnixxc@gmail.com>
2025-11-23 16:47:59 -0500

    add banner inside config
    fmt

--- home/config/nvim/lua/plugins/configs/snacks.lua
+++ home/config/nvim/lua/plugins/configs/snacks.lua
@@ -1,38 +1,31 @@
 require("snacks").setup({
     dashboard = {
-        width = 60,
+        width = 70,
         pane_gap = 4,
         preset = {
-            header = [[ 
-
-
-
-
-            ]],
+            header = [[ ]],
             keys = {
                 { icon = "􀊫", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
                 { icon = "􀑍", key = "n", desc = "New File", action = ":ene | startinsert" },
                 { icon = "􀕹", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
                 { icon = "􀣔", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-                { icon = "􀧏", key = "s", desc = "Restore Session", section = "session" },
-                { icon = "􀖃", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                -- { icon = "􀧏", key = "s", desc = "Restore Session", section = "session" },
+                -- { icon = "􀖃", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
                 { icon = "􀆄", key = "q", desc = "Quit", action = ":qa" },
             },
         },
         sections = {
             {
-                pane = 2,
+                pane = 1,
                 section = "terminal",
-                cmd = "chafa /Users/tnixc/Pictures/assets/Z2.PNG --format symbols --symbols vhalf --size 100x6 --stretch -t 1",
-                height = 6,
-                padding = 2,
-                indent = -52,
+                cmd = "echo '' && chafa ./assets/banner.jpg --format symbols --fg-only --symbols braille --size 70x25 --stretch",
+                height = 27,
+                padding = 0,
             },
-            { section = "header", pane = 1 },
-            { section = "keys", gap = 1, padding = 1 },
-            { pane = 2, icon = "􀣔", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-            { pane = 2, icon = "􀙅", title = "Projects", section = "projects", indent = 2, padding = 1 },
-            { section = "startup", indent = 60, padding = 5, pane = 1, icon = "􀋥  " },
+            { section = "keys", gap = 1, padding = 2, pane = 2 },
+            { pane = 2, icon = "􀣔", title = "Recent Files", section = "recent_files", indent = 2, padding = 2 },
+            { pane = 2, icon = "􀙅", title = "Projects", section = "projects", indent = 2, padding = 2 },
+            { section = "startup", padding = 1, pane = 2, icon = "􀋥  " },
         },
     },
     terminal = {
