0bba1243b46d69c3feacfbd55d7d084a9f8a3242
Tnixc <tnixxc@gmail.com>
2025-12-10 20:09:08 -0500

    git good

--- home/tnixc/config/nvim/lua/plugins/configs/treesitter.lua
+++ home/tnixc/config/nvim/lua/plugins/configs/treesitter.lua
@@ -31,18 +31,3 @@ configs.setup({
         enable_autocmd = true,
     },
 })
-
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.elle = {
-    install_info = {
-        url = "/Users/tnixc/Developer/tree-sitter-elle", -- local path or git repo
-        files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-        -- optional entries:
-        branch = "mistress", -- default branch in case of git repo if different from master
-        generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-        requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-    },
-    filetype = "le", -- if filetype does not match the parser name
-}
-
-vim.treesitter.language.register("elle", "le") -- the someft filetype will use the python parser and queries.
