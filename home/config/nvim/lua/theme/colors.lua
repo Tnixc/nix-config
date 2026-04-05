574de88dcb85f6b61a730a30cc52d91ff8f9b5ce
Tnixc <tnixxc@gmail.com>
2026-04-05 19:35:52 -0400

    updates

--- home/config/nvim/lua/theme/colors.lua
+++ home/config/nvim/lua/theme/colors.lua
@@ -1,1 +1,1 @@
-/nix/store/3pww3z6nkmp8i3j5lgvasws91ymsxcky-home-manager-files/nix-config/home/config/nvim/lua/theme/colors.lua
+/nix/store/dv3b8v93nj57pfzfnfixszmcf06c9qfy-home-manager-files/nix-config/home/config/nvim/lua/theme/colors.lua
